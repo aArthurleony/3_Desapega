26f28743e842849e1ac2b4db92eaf2e80b506039
Wendel <wendel824829has@gmail.com>
2024-07-29 08:28:17 -0300

    rota 404

--- src/config/conn.js
+++ src/config/conn.js
@@ -2,11 +2,11 @@ import mysql from "mysql2";
 
 const conn = mysql.createPool({
   connectionLimit: 10,
-  host: "localhost",
-  user: "rooot",
-  password: "Sen@iDev77!.",
-  database: "desapega",
-  port: 3306,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT,
 });
 
 conn.query("Select 1 + 1 AS solution", (err, result, fields) => {
--- src/server.js
+++ src/server.js
@@ -4,14 +4,18 @@ import express from "express";
 const PORT = process.env.PORT;
 
 //importar conexçao
-import conn from "./config/conn.js"
+import conn from "./config/conn.js";
 
+//404
 const app = express();
 
 app.get("/", (request, response) => {
   response.send("Olá mundo!");
 });
 
+app.use((request, response) => {
+  response.status(404).json({ message: "Rota nao encontrada" });
+});
 app.listen(PORT, () => {
   console.log("Servidor on PORT: " + PORT);
 });
