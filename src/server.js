4ceac6cc9c21c76cbbd2e09befbff3c120a78b8b
Wendel <wendel824829has@gmail.com>
2024-08-02 10:06:39 -0300

    add

--- src/server.js
+++ src/server.js
@@ -1,25 +1,34 @@
 import "dotenv/config";
 import express from "express";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 const PORT = process.env.PORT;
 
-//importar conexçao
+//*importar conexçao
 import conn from "./config/conn.js";
 
-//importacao dos modulos TABELA
-import "./models/usuariomodel.js"
+//*importacao dos modulos TABELA
+import "./models/usuariomodel.js";
 
-//Importar as rotas
-import usuarioRouter from "./routes/usuarioroutes.js"
+//*Importar as rotas
+import usuarioRouter from "./routes/usuarioroutes.js";
 
 const app = express();
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())   
 
-//utilizar as rotas
-app.use("/usuarios", usuarioRouter)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-//404
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//*localizar onde está a pasta public
+app.use("/public", express.static(path.join(__dirname, "public")));
+
+//*utilizar as rotas
+app.use("/usuarios", usuarioRouter);
+
+//*404
 app.use((request, response) => {
   response.status(404).json({ message: "Rota nao encontrada" });
 });
