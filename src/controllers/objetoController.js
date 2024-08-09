ea3d89b4f44e38d7ddac0eaff64beefd00c83a9e
Wendel <wendel824829has@gmail.com>
2024-08-09 07:51:28 -0300

    ik

--- src/controllers/objetoController.js
+++ src/controllers/objetoController.js
@@ -2,9 +2,9 @@ import conn from "../config/conn.js";
 import { v4 as uuidv4 } from "uuid";
 import getToken from "../helpers/get-token.js";
 import getUserByToken from "../helpers/get-user-by-token.js";
-import { request } from "express";
+import { response } from "express";
 
-export const create = async (reques, response) => {
+export const create = async (request, response) => {
   const { nome, peso, cor, descricao } = request.body;
   const disponivel = 1;
 
@@ -16,15 +16,15 @@ export const create = async (reques, response) => {
     return;
   }
   if (!peso) {
-    response.status(400).json("o nome do objeto é obrigatório");
+    response.status(400).json("o peso do objeto é obrigatório");
     return;
   }
   if (!cor) {
-    response.status(400).json("o nome do objeto é obrigatório");
+    response.status(400).json("o cor do objeto é obrigatório");
     return;
   }
   if (!descricao) {
-    response.status(400).json("o nome do objeto é obrigatório");
+    response.status(400).json("o descricao do objeto é obrigatório");
     return;
   }
   const objeto_id = uuidv4();
@@ -59,15 +59,56 @@ export const create = async (reques, response) => {
       const imageValues = request.file.map((file) => [
         uuidv4(),
         objeto_id,
-        file.filename
+        file.filename,
       ]);
-      conn.query(insertImageSQL, [imageValues], (err)=>{
-        if(err){
-            console.error(err)
-            response.status(500).json({err: "Erro ao salvar imagens do objeto"})
-            return
+      conn.query(insertImageSQL, [imageValues], (err) => {
+        if (err) {
+          console.error(err);
+          response
+            .status(500)
+            .json({ err: "Erro ao salvar imagens do objeto" });
+          return;
         }
-      })
+        response.status(201).json("Objeto cadastrado com sucesso!");
+      });
+    } else {
+      response.status(201).json("objeto cadastrado com sucesso");
     }
   });
+};
+
+export const getAllObjectUser = async (request, response) => {
+  try {
+    const token = getToken(request);
+    const user = await getUserByToken(token);
+
+    const usuarioId = user.usuario_id;
+    const selectSQL = /*sql*/ `
+    SELECT
+    obj.objeto_id,
+    obj.usuario_id,
+    obj.nome,
+    obj.peso,
+    obj.cor,
+    obj.descricao,
+    GROUP_CONCAT(obj_img.image_path SEPARATOR ',') AS image_path
+    FROM
+        objetos AS obj
+    LEFT JOIN
+        objeto_images AS obj_img ON obj.objeto_id = obj_img.objeto_id
+    WHERE
+        obj.usuario_id = ?
+    GROUP BY
+        obj.objeto_id, obj.usuario_id, obj.nome, obj.peso, obj.cor, obj.descricao
+    `;
+    conn.query(selectSQL, [usuarioId], (err, data)=>{
+      if(err){
+        console.error(err)
+        response.status(500).json({err: "erro ao buscar objeto"})
+        return
+      }
+      const objetoUsuario = data
+      response.status(200).json(objetoUsuario)
+    })
+  } catch (error) {}
 };
--- src/routes/objetoRouter.js
+++ src/routes/objetoRouter.js
@@ -4,8 +4,14 @@ import { create } from "../controllers/objetoController.js";
 //*helpers
 import verifyToken from "../helpers/verify-token.js";
 import imageUpload from "../helpers/image-upload.js";
+import { getAllObjectUser } from "../controllers/objetoController.js";
 
 const router = Router();
 router.post("/create", verifyToken, create, imageUpload.array("imagens", 10), create);
+//*listar todos os objetos
+//*resgatar objetos pelo id
+//*listar as imagens de um objeto
+//*listar todas as imagens que pertencem a um usuario
+router.get("usuarios/imagens", verifyToken, getAllObjectUser)
 
 export default router;
